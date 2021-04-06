d51a45c17083959e42a450f6c932af36d64dea9f
John Wu <jhnwu3@gmail.com>
2021-04-06 17:13:21 -0400

    $

--- ODE.cpp
+++ ODE.cpp
@@ -13,7 +13,7 @@ typedef boost::array< double , 3 > state_type;
 typedef runge_kutta_cash_karp54< double > stepper_type;
 void tripleNonlinearODE( const state_type &x , state_type &dxdt , double t )
 {
-    dxdt[0] =  ( x[0] - x[1] ); // dx /dt = x + 4z
+    dxdt[0] =   x[0] + 4*x[2]; // dx /dt = x + 4z
     dxdt[1] =  2*x[1]; // dy / dt = 2y
     dxdt[2] =  3*x[0] + x[1] - 3*x[2]; // dz / dt = 3x + y - 3z
 }
