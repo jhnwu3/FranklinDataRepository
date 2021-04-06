d8e61730348b810d386a68b730f87489e1003d7c
John Wu <jhnwu3@gmail.com>
2021-04-06 16:58:19 -0400

    modified to now do runge_kutta_cash_karp54

--- ODE.cpp
+++ ODE.cpp
@@ -6,23 +6,20 @@
 using namespace std;
 using namespace boost::numeric::odeint;
 
-const double sigma = 10.0;
-const double R = 28.0;
-const double b = 8.0 / 3.0;
+
  ofstream oFile; 
 
 typedef boost::array< double , 3 > state_type;
-
-void lorenz( const state_type &x , state_type &dxdt , double t )
+typedef runge_kutta_cash_karp54< double > stepper_type;
+void tripleNonlinearODE( const state_type &x , state_type &dxdt , double t )
 {
-    dxdt[0] = sigma * ( x[1] - x[0] );
-    dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
-    dxdt[2] = -b * x[2] + x[0] * x[1];
+    dxdt[0] =  ( x[0] - x[1] ); // dx /dt = x + 4z
+    dxdt[1] =  2*x[1]; // dy / dt = 2y
+    dxdt[2] =  3*x[0] + x[1] - 3*x[2]; // dz / dt = 3x + y - 3z
 }
 
-void write_lorenz( const state_type &x , const double t )
+void write_file( const state_type &x , const double t )
 {
-    
     
     oFile << t << ',' << x[0] << ',' << x[1] << ',' << x[2] << endl;
 }
@@ -31,6 +28,7 @@ int main(int argc, char **argv)
 {
     oFile.open("ODE_Soln.csv");
     state_type x = { 10.0 , 1.0 , 1.0 }; // initial conditions
-    integrate( lorenz , x , 0.0 , 25.0 , 0.1 , write_lorenz );
+    integrate_adaptive( make_controlled( 1E-12 , 1E-12 , stepper_type() ) ,
+                        tripleNonlinearODE , x , 1.0 , 10.0 , 0.1 , write_file );
     oFile.close();
 }
