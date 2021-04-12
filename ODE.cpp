57cbee10f8f765c9c10924e0295f577b21ef44cf
John Wu <jhnwu3@gmail.com>
2021-04-12 10:59:56 -0400

    $

--- ODE.cpp
+++ ODE.cpp
@@ -2,18 +2,32 @@
 #include <boost/array.hpp>
 #include <fstream>
 #include <boost/numeric/odeint.hpp>
-
+#include <vector>
 using namespace std;
 using namespace boost::numeric::odeint;
 
 
  ofstream oFile; 
-
+typedef boost::numeric::ublas::vector< double > vector_type;
+typedef boost::numeric::ublas::matrix< double > matrix_type;
+//typedef boost::vector <double> state_type;
 typedef boost::array< double , 3 > state_type;
-typedef runge_kutta_cash_karp54< double > stepper_type;
+typedef runge_kutta_cash_karp54< state_type > error_stepper_type;
+typedef controlled_runge_kutta< error_stepper_type > controlled_stepper_type;
+struct rhs_van 
+{
+    void operator()( const state_type &x , vector_type &dxdt , double /* t */ )
+    {
+        dxdt[0] =  x[0] + 4*x[2]; // dx /dt = x + 4z
+        dxdt[1] =  2*x[1]; // dy / dt = 2y
+        dxdt[2] =  3*x[0] + x[1] - 3*x[2]; // dz / dt = 3x + y - 3z
+    }
+};
+
+
 void tripleNonlinearODE( const state_type &x , state_type &dxdt , double t )
 {
-    dxdt[0] =   x[0] + 4*x[2]; // dx /dt = x + 4z
+    dxdt[0] =  x[0] + 4*x[2]; // dx /dt = x + 4z
     dxdt[1] =  2*x[1]; // dy / dt = 2y
     dxdt[2] =  3*x[0] + x[1] - 3*x[2]; // dz / dt = 3x + y - 3z
 }
@@ -26,9 +40,13 @@ void write_file( const state_type &x , const double t )
 
 int main(int argc, char **argv)
 {
+    
     oFile.open("ODE_Soln.csv");
-    state_type x = { 10.0 , 1.0 , 1.0 }; // initial conditions
-    //integrate_adaptive( make_controlled( 1E-12 , 1E-12 , stepper_type() ) ,
-      //                  tripleNonlinearODE , x , 1.0 , 10.0 , 0.1 , write_file );
+    /*state_type x = { 10.0 , 1.0 , 1.0 }; // initial conditions
+    integrate( tripleNonlinearODE, x, 0.0, 10.0, 0.1, write_file);*/
+    state_type x0 = { 10.0 , 1.0 , 1.0 };
+ 
+    controlled_stepper_type controlled_stepper;
+    integrate_adaptive(controlled_stepper,tripleNonlinearODE,x0, 1.0, 10.0, 0.01, write_file);
     oFile.close();
 }
