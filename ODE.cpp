eadcc4a43eda5e75fa8b574ebe611da9cb52999b
John Wu <jhnwu3@gmail.com>
2021-04-12 11:41:44 -0400

    $

--- ODE.cpp
+++ ODE.cpp
@@ -27,9 +27,9 @@ struct rhs_van
 
 void tripleNonlinearODE( const state_type &x , state_type &dxdt , double t )
 {
-    dxdt[0] =  x[0] + 4*x[2]; // dx /dt = x1*x2 - x0
-    dxdt[1] =  2*x[1]*x[0]; // dy / dt = 2y*x
-    dxdt[2] =  3*x[0] + x[1] - 3*x[2]; // dz / dt = 3x + y - 3z
+    dxdt[0] =  -0.04*x[0] + 10000*x[1] *x[2]; // dx1 /dt = -.04 * x1 + 10^4 * x2*x3
+    dxdt[1] =  0.04*x[0] - 10000 * x[1]*x[2]-3*10e7*x[1]*x[1]; // dx2/dt = 0.04*x1 - 10^4 * x2*x3 - 3 *10^7 * x2^2
+    dxdt[2] =  3*10e7*x[1]*x[1]; // dx3 / dt = 3* 10^7*x2^2
 }
 
 void write_file( const state_type &x , const double t )
@@ -46,6 +46,6 @@ int main(int argc, char **argv)
     integrate( tripleNonlinearODE, x, 0.0, 10.0, 0.1, write_file);*/
     state_type x0 = {1.0 , 0.0 , 0.0 };
     controlled_stepper_type controlled_stepper;
-    integrate_adaptive(controlled_stepper,tripleNonlinearODE,x0, 1.0, 10.0, 0.01, write_file);
+    integrate_adaptive(controlled_stepper,tripleNonlinearODE,x0, 0.0, 10.0, 0.01, write_file);
     oFile.close();
 }
