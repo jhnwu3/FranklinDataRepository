473a66b70747a718030f540945319769a09b7eda
John Wu <jhnwu3@gmail.com>
2021-05-04 15:55:08 -0400

    $

--- var.cpp
+++ var.cpp
@@ -4,9 +4,9 @@
 double ke = 0.0001, kme = 20, kf = 0.01, kmf = 18, kd = 0.03, kmd = 1, 
 ka2 = 0.01, ka3 = 0.01, C1T = 20, C2T = 5, C3T = 4;
 
-/* Number of proteins */
-int N = 10000; 
+/* Number of proteins & columns/diff eq solutions for ODEs */
+int N = 10000, pCol = 4; 
 /* time conditions, t0 = start time, tf = final time, dt = time step*/
 double t0 = 0.0, tf = 500.0, dt = 10.0;
 /* Matrix to fill, initialized to 0, 50 rows, 4 columns */
-MatrixXd pAvg( (int) tf / 10, 4) = MatrixXd::Zero( (int) tf / 10, 4);
+MatrixXd pAvg = MatrixXd::Zero((int) tf / dt, pCol);
