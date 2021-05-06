1db1c47ac7dd5ec2523874bbccc11cdec1a0e84e
John Wu <jhnwu3@gmail.com>
2021-05-06 18:21:56 -0400

    $

--- ODE.cpp
+++ ODE.cpp
@@ -86,6 +86,9 @@ int main(int argc, char **argv)
 
     /* average randomized sample/initial conditions from unif dist, N=10,000 */
    for(int i = 0; i < N; i++){
+       if(i % 200 == 0){
+           cout << "At i =" << i << endl; 
+       }
        c0 = {xNorm(generator), yNorm(generator), zNorm(generator)};
        integrate_const(controlled_stepper, tripleNonlinearODE, c0, t0, tf, dt, sample_const);
    }
