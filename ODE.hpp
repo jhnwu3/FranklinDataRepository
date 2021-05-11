71751c2cba6887655f5b14f8cae321afbfae8150
John Wu <jhnwu3@gmail.com>
2021-05-11 17:29:56 -0400

    $

--- ODE.hpp
+++ ODE.hpp
@@ -7,7 +7,7 @@
 #include <random>
 #include <cmath>
 #include <chrono>
-#define NPROTEINS 6
+#define NPROTEINS 3
 /* namespaces for ease of use */
 using namespace std;
 using namespace boost::numeric::odeint;
