e96c3711b5276bc4cc011fac20f119caf9b449d9
John Wu <jhnwu3@gmail.com>
2021-05-12 15:58:45 -0400

    $

--- ODE.cpp
+++ ODE.cpp
@@ -74,10 +74,10 @@ int main(int argc, char **argv)
     controlled_stepper_type controlled_stepper;
 
     /* assign mu vector and sigma matrix values */
-    /*mu << mu_x, mu_y, mu_z;
+    mu << mu_x, mu_y, mu_z;
     sigma << 0.77, 0.0873098, 0.046225, 
              0.0873098, 0.99, 0.104828, 
-             0.046225, 0.104828, 1.11; */
+             0.046225, 0.104828, 1.11; 
     
     /*for(int row = 0; row < N_SPECIES; row++){
          mu(row) = 1 + unifDist(generator);
