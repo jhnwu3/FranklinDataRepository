07c29b731a0b306d448732a116b33c93fe1ca60e
John Wu <jhnwu3@gmail.com>
2021-05-19 13:59:57 -0400

    $

--- para.cpp
+++ para.cpp
@@ -8,20 +8,38 @@
 #include <cmath>
 #include <chrono>
 #include <omp.h>
+#define N_SPECIES 3 // using #defines technically right now, but will eventually change it to a variable in main
+#define N 10
+#define N_DIM 5
 
-#define N_PARTICLES 5
+
+/* namespaces for ease of use */
 using namespace std;
-void printHello(){
-    cout << "Hello" << endl;
-}
-int main (){
-    int i;
-    cout << "Before par. for " << endl;
+using namespace Eigen;
+using namespace boost::numeric::odeint;
 
-#pragma omp parallel
+/* typedefs for boost ODE-ints*/
+typedef boost::numeric::ublas::vector< double > vector_type;
+typedef boost::numeric::ublas::matrix< double > matrix_type;
+typedef boost::array< double , N_SPECIES > state_type;
+typedef runge_kutta_cash_karp54< state_type > error_stepper_type;
+typedef controlled_runge_kutta< error_stepper_type > controlled_stepper_type;
+
+
+
+
+
+int main (){
+
+    cout << "Before par. for " << endl;
+    int sum = 0;
+#pragma omp parallel for
 {
-    cout << "Printing from thread:" << omp_get_thread_num() << "index:" << i << endl; 
+    for(int i = 0; i < N; i++){
+        cout << "Printing from thread:" << omp_get_thread_num() << "index:" << i << endl; 
+        sum += i;
+    }
 }
-    
+    cout << endl << endl << "final sum: " << sum;
     return EXIT_SUCCESS;
 }
