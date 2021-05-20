da33b67402db0aa9fd680870a88a42932adb0dde
John Wu <jhnwu3@gmail.com>
2021-05-20 15:27:44 -0400

    $

--- para.cpp
+++ para.cpp
@@ -36,6 +36,7 @@ void doStuff(){
 int main (){
     cout << "Before par. for, max_threads: " << omp_get_max_threads << endl;
     int costs[N] = {1,2,4,5,6,7,8,9,10};  
+    for(int j = 0; j< 20000; j++){}
 #pragma omp parallel for
     for(i = 0; i < N; i++){
         doStuff();
