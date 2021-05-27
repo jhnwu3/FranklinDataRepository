e5b3bb1385518c76c9b669709dd787d5a88349bb
John Wu <jhnwu3@gmail.com>
2021-05-27 11:14:25 -0400

    $

--- slurm_run.sh
+++ slurm_run.sh
@@ -3,9 +3,8 @@
 #SBATCH --job-name=test
 #SBATCH --partition=general
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=20 
 #SBATCH --output=slurm.out
 set -e
 echo "This is a test run by John Wu"
-export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
+export OMP_NUM_THREADS=5
 ./ODE 
