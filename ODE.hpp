a0ae650699bcf4b4666cd54eb91699ab860688e2
John Wu <jhnwu3@gmail.com>
2021-06-07 11:29:01 -0400

    $

--- ODE.hpp
+++ ODE.hpp
@@ -94,17 +94,18 @@ struct Data_ODE_Observer
         if(t == tf){
             for(int row = 0; row < dComp.subset.size(); row++){ // first moments of specified subset
                 int i = dComp.subset(row) - 1; // i.e subset = {1,2,3} = index = {0,1,2}
-                if(i >= 0){ dComp.moments(i) +=  c[i]; }
-                for(int col = row; col < dComp.subset.size(); col++){
-                    int j = dComp.subset(col) - 1;
-                    if (j >= 0){
-                        if( i == j ){
-                            dComp.moments(N_SPECIES + i) += c[i] * c[j];
-                        }else{
-                            dComp.moments(2*N_SPECIES + (i + j - 1)) += c[i] *c[j];
+                if(i >= 0){ dComp.moments(i) +=  c[i]; 
+                    for(int col = row; col < dComp.subset.size(); col++){
+                        int j = dComp.subset(col) - 1;
+                        if ( j >= 0 ){
+                            if( i == j ){
+                                dComp.moments(N_SPECIES + i) += c[i] * c[j];
+                            }else{
+                                dComp.moments(2*N_SPECIES + (i + j - 1)) += c[i] *c[j];
+                            }
+                            dComp.secondMoments(i,j) += (c[i] * c[j]);   // store in a 2nd moment matrix
+                            dComp.secondMoments(j,i) = dComp.secondMoments(i,j);   // store in a 2nd moment matrix
                         }
-                        dComp.secondMoments(i,j) += (c[i] * c[j]);   // store in a 2nd moment matrix
-                        dComp.secondMoments(j,i) = dComp.secondMoments(i,j);   // store in a 2nd moment matrix
                     }
                 }
             }
@@ -120,17 +121,18 @@ struct Data_ODE_Observer6
         if(t == tf){
             for(int row = 0; row < dComp.subset.size(); row++){ // first moments of specified subset
                 int i = dComp.subset(row) - 1; // i.e subset = {1,2,3} = index = {0,1,2}
-                if(i >= 0){ dComp.moments(i) +=  c[i]; }
-                for(int col = row; col < dComp.subset.size(); col++){
-                    int j = dComp.subset(col) - 1;
-                    if (j >= 0){
-                        if( i == j ){
-                            dComp.moments(N_SPECIES + i) += c[i] * c[j];
-                        }else{
-                            dComp.moments(2*N_SPECIES + (i + j - 1)) += c[i] *c[j];
+                if(i >= 0){ dComp.moments(i) +=  c[i]; 
+                    for(int col = row; col < dComp.subset.size(); col++){
+                        int j = dComp.subset(col) - 1;
+                        if ( j >= 0 ){
+                            if( i == j ){
+                                dComp.moments(N_SPECIES + i) += c[i] * c[j];
+                            }else{
+                                dComp.moments(2*N_SPECIES + (i + j - 1)) += c[i] *c[j];
+                            }
+                            dComp.secondMoments(i,j) += (c[i] * c[j]);   // store in a 2nd moment matrix
+                            dComp.secondMoments(j,i) = dComp.secondMoments(i,j);   // store in a 2nd moment matrix
                         }
-                        dComp.secondMoments(i,j) += (c[i] * c[j]);   // store in a 2nd moment matrix
-                        dComp.secondMoments(j,i) = dComp.secondMoments(i,j);   // store in a 2nd moment matrix
                     }
                 }
             }
