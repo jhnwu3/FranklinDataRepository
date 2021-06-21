c28d5157a0389a7662788727332be32d4f651888
John Wu <jhnwu3@gmail.com>
2021-06-21 12:16:05 -0400

    $

--- PSO_S_ODE.cpp
+++ PSO_S_ODE.cpp
@@ -625,9 +625,8 @@ int main() {
 		cost_sofar = cost_seedk;
 		cout << "ln 626" << endl;
 		GBMAT.conservativeResize(1, 6);
-		
 		// will probably find a better method later, but will for now just create temp vec to assign values.
-		
+		cout << "breaks here" << endl;
 		VectorXd cbind(gbest.size() + 1);
 		cbind << gbest, cost_gbest;
 		GBMAT.row(GBMAT.rows() - 1) = cbind;
