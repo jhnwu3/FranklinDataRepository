9a1480c4cdf97ecfde7fb425c3a31c6121326fdf
John Wu <jhnwu3@gmail.com>
2021-06-21 12:09:57 -0400

    $

--- PSO_S_ODE.cpp
+++ PSO_S_ODE.cpp
@@ -569,6 +569,7 @@ int main() {
 				// }
 				c0[j] = X_0_db(i,j);
 			}
+			cout << "ln 572" << endl;
 			integrate_adaptive(controlledStepper, nonlinODE6, c0, t0, tf, dt, obs); 
 			Q.row(i) = dCom.mat.row(i);
 		}
