a995e62a87fc2fa0e697fc1754d96b752aad2df8
John Wu <jhnwu3@gmail.com>
2021-06-23 11:38:29 -0400

    $

--- ODE.cpp
+++ ODE.cpp
@@ -2,7 +2,9 @@
 #include "ODE.hpp"
 
 void nonlinearODE3( const State_N &c , State_N &dcdt , double t )
-{
+{   
+    double ke = 0.0001, kme = 20, kf = 0.01, kmf = 18, kd = 0.03, kmd = 1,
+    ka2 = 0.01, ka3 = 0.01, C1T = 20, C2T = 5, C3T = 4;
     dcdt[0] =  ((ke*(C1T - c[0]))/(kme + (C1T - c[0]))) + ((kf * (C1T - c[0]) * c[0] * c[1]) / (kmf + (C1T - c[0]))) - ((kd*c[0]*c[2])/(kmd + c[0])); // dc1dt = ke*(C1T-C1).... (in document)
     dcdt[1] =  ka2 *(C2T - c[1]); // dc2/dt = ka2 * (C2T - c2)
     dcdt[2] =  ka3*(C3T - c[2]); // dc3/dt = ka3 * (C3t - c3)
@@ -35,19 +37,5 @@ void linearODEn_1( const State_N &c , State_N &dcdt , double t )
         j++; 
     }
 }
-/* Test for 6 systems */
-void nonlinearODE6( const State_N &c , State_N &dcdt , double t){
-    dcdt[0] =  ((ke*(C1T - c[0])) / (kme + (C1T - c[0]))) + 
-               ((kf * (C1T - c[0]) * c[0] * c[1]) / (kmf + (C1T - c[0]))) - 
-               ((kd*c[0]*c[2]) / (kmd + c[0])) +
-               ((kf * (C1T - c[0]) * c[0] * c[3]) / (kmf + (C1T - c[0])))- 
-               ((kd*c[0]*c[4]) / (kmd + c[0])) +
-               ((kf * (C1T - c[0]) * c[0] * c[5]) / (kmf + (C1T - c[0]))); // dc1dt = ke*(C1T-C1).... (in document)
 
-    dcdt[1] =  ka2 *(C2T - c[1]); // dc2/dt = ka2 * (C2T - c2)
-    dcdt[2] =  ka3*(C3T - c[2]); // dc3/dt = ka3 * (C3t - c3)
-    dcdt[3] =  ka3*(C3T - c[3]); // dc3/dt = ka3 * (C3t - c3)
-    dcdt[4] =  ka3*(C3T - c[4]); // dc3/dt = ka3 * (C3t - c3)
-    dcdt[5] =  ka3*(C3T - c[5]); // dc3/dt = ka3 * (C3t - c3)
-}
 
