218765bbfd8af9433e20cb953fbb2545f7bfe63e
John Wu <jhnwu3@gmail.com>
2021-06-24 16:11:52 -0400

    $

--- main.cpp
+++ main.cpp
@@ -110,11 +110,11 @@ int main(int argc, char **argv)
     Data_ODE_Observer t5Obs6(time_5); // obs sums over subset of values
     Data_Components time_10(sub, 10.0, nMom); // Y_t = mu
     Data_ODE_Observer t10Obs6(time_10); // obs sums over subset of values
-    State_N c0 = gen_multi_lognorm_init6(); //gen_multi_lognorm_init6();
+    State_N c0t = gen_multi_lognorm_init6(); //gen_multi_lognorm_init6();
     for(int i = 0; i < N; i++){
-        integrate_adaptive(controlled_stepper, ode6Sys, c0, t0, tf, dt, tfObs6);
-        integrate_adaptive(controlled_stepper, ode6Sys, c0, t0, 5.0, dt, t5Obs6);
-        integrate_adaptive(controlled_stepper, ode6Sys, c0, t0, 10.0, dt, t10Obs6);
+        integrate_adaptive(controlled_stepper, ode6Sys, c0t, t0, tf, dt, tfObs6);
+        integrate_adaptive(controlled_stepper, ode6Sys, c0t, t0, 5.0, dt, t5Obs6);
+        integrate_adaptive(controlled_stepper, ode6Sys, c0t, t0, 10.0, dt, t10Obs6);
     }
     time_tf.secondMoments/=N; // average moments
     time_tf.moments /= N;
