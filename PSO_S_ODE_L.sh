846a70d8359b0d44cdeef0c5e4e27b926b8d28c6
John Wu <jhnwu3@gmail.com>
2021-06-24 16:40:10 -0400

    $

--- PSO_S_ODE_L.sh
+++ PSO_S_ODE_L.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name=PSO_ODE_Linear
 #SBATCH --partition=general
 #SBATCH --nodes=1
-#SBATCH --output=slurm_PSO_S_ODE_L.out
 #SBATCH --ntasks-per-node=1
 set -e
 echo "PSO_S_ODE_NonLinear"
