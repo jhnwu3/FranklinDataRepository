329aa15f2d3917a0c3d0fdfc0ac887d231b1076c
John Wu <jhnwu3@gmail.com>
2021-06-29 15:51:13 -0400

    $

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
 #pragma omp parallel for
     for(particle = 0; particle < N_PARTICLES; particle++){
        
-        int nSteps = 400;
+        int nSteps = 50;
         struct K pos; // particle k vals
         /* rng */
         random_device pRanDev;
