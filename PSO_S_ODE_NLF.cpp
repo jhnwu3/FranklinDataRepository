67d3ce22d8add68d0b9dc93df1a6d6b247d3a8ce
John Wu <jhnwu3@gmail.com>
2021-07-03 14:56:23 -0400

    $

--- PSO_S_ODE_NLF.cpp
+++ PSO_S_ODE_NLF.cpp
@@ -14,7 +14,7 @@
 #include <chrono>
 
 #define N_SPECIES 6
-#define N 10 // # of samples to sample over
+#define N 100 // # of samples to sample over
 #define N_DIM 6 // dim of PSO hypercube
 #define N_PARTICLES 20 
 
