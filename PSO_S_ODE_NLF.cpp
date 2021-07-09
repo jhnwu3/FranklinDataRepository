becfd017178177b00b09d5444b6439886f5238db
John Wu <jhnwu3@gmail.com>
2021-07-09 19:40:49 -0400

    $

--- PSO_S_ODE_NLF.cpp
+++ PSO_S_ODE_NLF.cpp
@@ -505,7 +505,7 @@ int main() {
             truCp2.k(j) = unifDist(gen);
         }
         cout << "k:" << truCp2.k.transpose() << endl;
-        Nonlinear_ODE6 trueSysCp2(truCp1);
+        Nonlinear_ODE6 trueSysCp2(truCp2);
         Protein_Moments YtCp2(tf, nMoments);
         Mom_ODE_Observer YtObsCp2(YtCp1);
         for (int i = 0; i < N; i++) {
