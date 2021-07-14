0a1bc71a8b84a04cd2801db4029e73b3faa8cee7
John Wu <jhnwu3@gmail.com>
2021-07-13 21:55:13 -0400

    $

--- para.cpp
+++ para.cpp
@@ -417,7 +417,7 @@ struct Write_File_Plot
 int main (){
     double mu_x = 1.47, mu_y = 1.74, mu_z = 1.99; // true means for MVN(theta)
     // ode vars
-    double t0 = 0.0, tf = 6.0, dt = 1.0;
+    double t0 = 0.0, tf = 10.0, dt = 1.0;
     struct K tru;
     tru.k = VectorXd::Zero(6);
     tru.k << 5.0, 0.1, 1.0, 8.69, 0.05, 0.70;
