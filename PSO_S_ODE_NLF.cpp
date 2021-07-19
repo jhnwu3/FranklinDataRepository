02a251ba50d9f41c3f139a95fa017fd05fd9e544
John Wu <jhnwu3@gmail.com>
2021-07-19 08:10:34 -0400

    $

--- PSO_S_ODE_NLF.cpp
+++ PSO_S_ODE_NLF.cpp
@@ -457,7 +457,7 @@ int main() {
     int sf2 = 1;
 
     int Nparts = 300;
-    int Nsteps = 40;
+    int Nsteps = 1;
     
     cout << "sample size:" << N << " Nparts:" << Nparts << " Nsteps:" << Nsteps << endl;
     /* moments */
@@ -538,7 +538,7 @@ int main() {
     /* PSO costs */
     double gCost = 20000;
     /* Instantiate seedk aka global costs */
-    for (int i = 0; i < Npars; i++) { seed.k(i) = unifDist(gen); }
+    for (int i = 0; i < Npars; i++) { seed.k(i) = tru.k(i); }
    
     Protein_Moments Xt(tf, nMoments);
     Mom_ODE_Observer XtObs(Xt);
@@ -577,7 +577,7 @@ int main() {
         struct K pos;
         pos.k = VectorXd::Zero(Npars);
         for(int i = 0; i < Npars; i++){
-            pos.k(i) = pUnifDist(pGenerator);
+            pos.k(i) = tru.k(i);
         }
         
         /* using new rate constants, instantiate particle best values */
