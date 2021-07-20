4f9794e6b907eb45a0edd881488e8f49a649319a
John Wu <jhnwu3@gmail.com>
2021-07-20 11:38:16 -0400

    $

--- PSO_S_ODE_NLF.cpp
+++ PSO_S_ODE_NLF.cpp
@@ -628,6 +628,7 @@ for(int step = 0; step < Nsteps; step++){
             /* update gBest and pBest */
             #pragma omp critical
             {
+                cout << "Steps:" << step << endl;
                 if(cost < PBMAT(particle, Npars)){ // particle best cost
                     for(int i = 0; i < Npars; i++){
                         PBMAT(particle, i) = pos.k(i);
