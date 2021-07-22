bd96385b8c2b37fa5cfc1502cdd47149cc001c8e
John Wu <jhnwu3@gmail.com>
2021-07-22 14:11:09 -0400

    $

--- para.cpp
+++ para.cpp
@@ -475,7 +475,7 @@ int main (){
     ofstream costOut;
     costOut.open("RateDist_vs_Cost.csv");
     int numDataPts = 100;
-    int sampleSize = 100;
+    int sampleSize = 10;
     double alpha = 0.001;
     for(int pt = 0; pt < numDataPts; pt++){
         struct K pos;
