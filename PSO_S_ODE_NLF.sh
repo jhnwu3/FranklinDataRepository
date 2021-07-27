7c79e8ae968ff4db865b2a2aa7698c66d777045b
John Wu <jhnwu3@gmail.com>
2021-07-27 11:58:39 -0400

    $

--- PSO_S_ODE_NLF.sh
+++ PSO_S_ODE_NLF.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --time=0-80:10:00 
 #SBATCH --job-name=PSO_ODE_Nonlinear
-#SBATCH --partition=general
 #SBATCH --nodes=1
 #SBATCH --output=./slurm_outputs/NLF%j.txt
-#SBATCH --ntasks-per-node=30
+#SBATCH --ntasks-per-node=20
 set -e
 echo "PSO_S_ODE_NonLinear"
 ./PSO_S_ODE_NLF
