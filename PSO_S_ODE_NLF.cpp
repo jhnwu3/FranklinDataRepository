7977d57fec67d573fb01913071052da25f568c7a
John Wu <jhnwu3@gmail.com>
2021-07-27 13:24:33 -0400

    $

--- PSO_S_ODE_NLF.cpp
+++ PSO_S_ODE_NLF.cpp
@@ -473,7 +473,7 @@ int main() {
     int sf2 = 1;
 
     // PSO run parameters
-    int Nparts = 100;
+    int Nparts = 1000;
     int Nsteps = 40;
     cout << "note: this run is using beta distribution of updating and is ran in para and is ran close to truk!" << endl;
     cout << "sample size:" << N << " Nparts:" << Nparts << " Nsteps:" << Nsteps << endl;
