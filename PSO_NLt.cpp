60f18bde5ef6935de6a569e7c3acd6da627c464f
John Wu <jhnwu3@gmail.com>
2021-08-06 12:34:51 -0400

    $

--- PSO_NLt.cpp
+++ PSO_NLt.cpp
@@ -477,7 +477,7 @@ int main() {
     double sfp = 3.0, sfg = 1.0, sfe = 6.0; // initial particle historical weight, global weight social, inertial
     double sfi = sfe, sfc = sfp, sfs = sfg; // below are the variables being used to reiterate weights
     double alpha = 0.2;
-    int N = 100;
+    int N = 25000;
     int nParts = 900; // first part PSO
     int nSteps = 15;
     int nParts2 = 25; // second part PSO
