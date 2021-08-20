693ca3cf93c9e85c6eee646e060b06c4588a895f
John Wu <jhnwu3@gmail.com>
2021-08-20 13:47:46 -0400

    $

--- PSO_NLt.cpp
+++ PSO_NLt.cpp
@@ -578,7 +578,7 @@ int main() {
     struct K seed;
     seed.k = VectorXd::Zero(Npars); 
     for (int i = 0; i < Npars; i++) { 
-        seed.k(i) = tru.k(i); //unifDist(gen);
+        seed.k(i) = unifDist(gen);
     }
     //seed.k << 0.725225, 0.0591037,  0.157054,  0.802015,  0.079692,  0.107293;
     Protein_Components Xt(tf, nMoments, N);
@@ -617,7 +617,7 @@ int main() {
             if(step == 0){
                 /* temporarily assign specified k constants */
                 for(int i = 0; i < Npars; i++){
-                    POSMAT(particle, i) = tru.k(i);//pUnifDist(pGenerator);//tru.k(i) + alpha * (0.5 - unifDist(pGenerator));
+                    POSMAT(particle, i) = pUnifDist(pGenerator);//tru.k(i) + alpha * (0.5 - unifDist(pGenerator));
                 }
                 //POSMAT.row(particle) <<  0.725225, 0.0591037,  0.157054,  0.802015,  0.079692,  0.107293;
                 struct K pos;
