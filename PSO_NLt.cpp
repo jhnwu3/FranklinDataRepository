86f3c1b1f94e97b7d0c42f1c7751b0c5ee1356fd
John Wu <jhnwu3@gmail.com>
2021-08-20 09:04:18 -0400

    $

--- PSO_NLt.cpp
+++ PSO_NLt.cpp
@@ -482,10 +482,10 @@ int main() {
     double sfi = sfe, sfc = sfp, sfs = sfg; // below are the variables being used to reiterate weights
     double alpha = 0.2;
     int N = 5000;
-    int nParts = 5; // first part PSO
-    int nSteps = 2;
-    int nParts2 = 60; // second part PSO
-    int nSteps2 = 10;
+    int nParts = 5000000; // first part PSO
+    int nSteps = 10;
+    int nParts2 = 100; // second part PSO
+    int nSteps2 = 200;
     int nMoments = (N_SPECIES * (N_SPECIES + 3)) / 2; // var + mean + cov
     int hone = 16;
     //nMoments = 2*N_SPECIES; // mean + var only!
