023548ebe10c70bf40dbc6410b2d3d3740da0ee1
John Wu <jhnwu3@gmail.com>
2021-08-23 17:31:44 -0400

    $

--- PSO_NLt.cpp
+++ PSO_NLt.cpp
@@ -482,7 +482,7 @@ int main() {
     double sfi = sfe, sfc = sfp, sfs = sfg; // below are the variables being used to reiterate weights
     double alpha = 0.2;
     int N = 5000;
-    int nParts = 50; // first part PSO
+    int nParts = 10; // first part PSO
     int nSteps = 2;
     int nParts2 = 50; // second part PSO
     int nSteps2 = 10;
