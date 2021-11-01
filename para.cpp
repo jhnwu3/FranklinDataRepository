7878ef0742c56fd343eaadcfbc897597ad708aa9
John Wu <jhnwu3@gmail.com>
2021-11-01 16:55:32 -0400

    $

--- para.cpp
+++ para.cpp
@@ -546,7 +546,7 @@ int main() {
     double sfp = 3.0, sfg = 1.0, sfe = 6.0; // initial particle historical weight, global weight social, inertial
     double sfi = sfe, sfc = sfp, sfs = sfg; // below are the variables being used to reiterate weights
     double alpha = 0.2;
-    int N = 500;
+    int N = 1000;
     int nParts = 125; // first part PSO
     int nSteps = 10;
     int nParts2 = 10; // second part PSO
