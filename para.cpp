6399911bc10ab02bc4ed066317f42b9dcd2745f7
John Wu <jhnwu3@gmail.com>
2021-11-01 15:11:27 -0400

    $

--- para.cpp
+++ para.cpp
@@ -550,7 +550,7 @@ int main() {
     int nParts = 25; // first part PSO
     int nSteps = 50;
     int nParts2 = 10; // second part PSO
-    int nSteps2 = 1000;
+    int nSteps2 = 100;
     int nMoments = (N_SPECIES * (N_SPECIES + 3)) / 2; // var + mean + cov
     int hone = 24;
     //nMoments = 2*N_SPECIES; // mean + var only!
@@ -998,6 +998,9 @@ int main() {
             cout << "New PBMAT:" << endl;
             cout << PBMAT << endl << endl;
         }
+        cout << "---------" << endl;
+        cout << "current:" << GBVEC.transpose() << endl;
+        cout << "cost: " << gCost << endl;
     }
     cout << "GBMAT after targeted PSO:" << endl << GBMAT << endl;
     trukCost = 0;
