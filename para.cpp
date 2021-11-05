44bb2df0fe518610721a1ad843b8668fe8247d87
John Wu <jhnwu3@gmail.com>
2021-11-05 11:59:28 -0400

    $

--- para.cpp
+++ para.cpp
@@ -550,10 +550,10 @@ int main() {
     double sfi = sfe, sfc = sfp, sfs = sfg; // below are the variables being used to reiterate weights
     double alpha = 0.2;
     int N = 5000;
-    int nParts = 200; // first part PSO
-    int nSteps = 20;
-    int nParts2 = 10; // second part PSO
-    int nSteps2 = 100;
+    int nParts = 100; // first part PSO
+    int nSteps = 10;
+    int nParts2 = 5; // second part PSO
+    int nSteps2 = 50;
     int nMoments = (N_SPECIES * (N_SPECIES + 3)) / 2; // var + mean + cov
     int hone = 32;
     //nMoments = 2*N_SPECIES; // mean + var only!
