3385f7c705590a8bb17b7a05db12c121dca9cde3
John Wu <jhnwu3@gmail.com>
2021-11-16 12:08:38 -0500

    $

--- para.cpp
+++ para.cpp
@@ -567,7 +567,7 @@ int main() {
     double alpha = 0.2;
     int nRuns = 1;
     int N = 500;
-    int nParts = 1000; // blind PSO  1000:10
+    int nParts = 100; // blind PSO  1000:10
     int nSteps = 10;
     int nParts2 = 1; // targeted PSO
     int nSteps2 = 2;
