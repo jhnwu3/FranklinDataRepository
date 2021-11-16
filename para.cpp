974af092c9b3b62ab315326898e2615b3a63d8e5
John Wu <jhnwu3@gmail.com>
2021-11-16 09:54:28 -0500

    $

--- para.cpp
+++ para.cpp
@@ -693,7 +693,7 @@ int main() {
     cout << "Reading in data!" << endl;
     /* Initial Conditions */
     int sizeFile = 25000;
-    int startRow = 4000;
+    int startRow = 4500;
     MatrixXd X_0_Full(sizeFile, Npars);
     MatrixXd Y_0_Full(sizeFile, Npars);
     MatrixXd X_0(N, Npars);
