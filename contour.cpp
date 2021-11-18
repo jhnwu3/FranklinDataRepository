0ac0ea4c594241c136928723a1c3688c6733707c
John Wu <jhnwu3@gmail.com>
2021-11-18 11:36:30 -0500

    $

--- contour.cpp
+++ contour.cpp
@@ -741,7 +741,7 @@ int main() {
         for(int y = 0; y < yDim; y++){
             K rate;
             rate.k = tru.k;
-            rate.k(1) = x / scale;
+            rate.k(0) = x / scale;
             rate.k(4) = y / scale;
             for(int t = 0; t < nTimeSteps; t++){
                 Nonlinear_ODE6 sys(rate);
