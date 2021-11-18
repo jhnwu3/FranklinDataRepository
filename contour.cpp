ce299ccededa6a25ffd6c20f5839e0fc97df5f62
John Wu <jhnwu3@gmail.com>
2021-11-18 11:34:14 -0500

    $

--- contour.cpp
+++ contour.cpp
@@ -508,7 +508,7 @@ int main() {
     double sfp = 3.0, sfg = 1.0, sfe = 6.0; // initial particle historical weight, global weight social, inertial
     double sfi = sfe, sfc = sfp, sfs = sfg; // below are the variables being used to reiterate weights
     double alpha = 0.2;
-    int N = 100;
+    int N = 25000;
     int nParts = 25; // first part PSO
     int nSteps = 50;
     int nParts2 = 10; // second part PSO
