55dfd0e4e6f88db3c1323d81fb5eff92d7fb9c24
John Wu <jhnwu3@gmail.com>
2022-01-28 12:13:39 -0500

    $

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=general
 #SBATCH --nodes=1
 #SBATCH --output=./slurm_outputs/test%j.txt
-#SBATCH --cpus-per-task=14
+#SBATCH --cpus-per-task=30
 # load all modules, build terminal code, move all outputs into output folders.
 
 ./para
