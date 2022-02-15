7f889a8eecec2633ac13d0649dbb3623b51fc49b
John Wu <jhnwu3@gmail.com>
2022-02-15 16:58:22 -0500

    $

--- contour.cpp
+++ contour.cpp
@@ -239,7 +239,7 @@ VectorXd gen_multi_lognorm_vecSub(void) {
     return initVec;
 }
 State_N convertInit(const MatrixXd& sample, int index){
-    State_N c0 = {sample(index,0), sample(index,1), 0, 0, sample(index,4), 0};
+    State_N c0 = {sample(index,0), sample(index,1), sample(index,2), sample(index,3)};
     return c0;
 }
 VectorXd comp_vel_vec(const VectorXd& posK, int seed, double epsi, double nan, int hone) {
